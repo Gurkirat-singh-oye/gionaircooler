535f8e441ceb1b289472e42fcb3066bc79276212
ThatOctoberMan <saggishm@gmail.com>
2025-09-05 22:37:11 +0530

    changes in about us

--- src/comps/AboutUs.js
+++ src/comps/AboutUs.js
@@ -13,7 +13,13 @@ import bulb from "../Images/icons/bulb.png";
 import qual from "../Images/icons/Quality.png";
 import cash from "../Images/icons/cash.png";
 import flower from "../Images/icons/Save energy.png";
-import abbanner from "../Images/aboutusimages/cf0313c87677119261c3e6a759d55d2ceff1865b.png"
+import abbanner from "../Images/aboutusimages/cf0313c87677119261c3e6a759d55d2ceff1865b.png";
+
+import oem from "../Images/aboutusimages/oemexec.jpg"
+import yrsexp from "../Images/aboutusimages/yearsexp.jpg"
+import rnd from "../Images/aboutusimages/randtool.jpg"
+import natn from "../Images/aboutusimages/nation.jpg"
+import happ from "../Images/aboutusimages/happycu.jpg"
 
 import Gbutton from "./utilcomps/Gbutton";
 import ContactUs from "./ContactUs";
@@ -25,10 +31,13 @@ function ContentAboutUs(params) {
       className={` my-10 relative ${
         window?.innerWidth < 1024 ? ` w-[300px] ` : ` w-[1340px] h-[600px] `
       } flex ${
-        window?.innerWidth < 1024 ? ` flex-col gap-8 ` : ` flex-row gap-24 `
+        window?.innerWidth < 1024 ? ` flex-col gap-8 ` : ` flex-row gap-16 `
       } `}
     >
-      <div className=" hidden lg:block absolute -left-[290px] top-64 h-20 text-nowrap text-[44px] font-black tracking-[0.4em] opacity-15 -rotate-90 " > A B O U T U S </div>
+      <div className=" hidden lg:block absolute -left-[290px] top-64 h-20 text-nowrap text-[44px] font-black tracking-[0.4em] opacity-15 -rotate-90 ">
+        {" "}
+        A B O U T U S{" "}
+      </div>
       <div
         className={` ${
           window?.innerWidth < 1024 ? ` h-[400px] ` : `  `
@@ -46,37 +55,58 @@ function ContentAboutUs(params) {
       >
         <div
           className={` text-center lg:text-left ${
-            window?.innerWidth < 768 ? `text-[28px]` : `text-6xl`
-          } text-gion-teal font-bold font-urbanist `}
+            window?.innerWidth < 768
+              ? `text-[28px]`
+              : `text-[55px] leading-none `
+          } text-gion-teal font-bold font-urbanist uppercase `}
         >
           Advanced Cooling for Ultimate Comfort
         </div>
         <div
           className={` text-[#495057] text-center lg:text-start ${
-            window?.innerWidth < 1024 ? ` text-xs ` : ` text-xl `
+            window?.innerWidth < 1024 ? ` text-xs ` : ` text-2xl `
           } `}
         >
-          <p className=" font-semibold ">Why Choose Gion Air Coolers?</p>
-          Gion Air Coolers don’t just cool—they supercool your space! Powered by
-          Aero Fan Technology, our coolers deliver strong air throw, enhanced
-          water retention, and superior cooling efficiency. With futuristic
-          smart features, you can control them from anywhere, anytime for
-          ultimate convenience.
+          {/* <p className=" font-semibold ">Why Choose Gion Air Coolers?</p> */}
+          Founded with a vision to make cooling more{" "}
+          <a className=" font-bold ">accessible</a>,{" "}
+          <a className=" font-bold ">efficient</a>, and{" "}
+          <a className=" font-bold ">reliable</a>,
+          <a className=" font-bold "> GionAirCooler</a> has grown into one of{" "}
+          <a className=" font-bold ">
+            India’s largest manufacturers and exporters of air coolers
+          </a>
+          . With over three decades of expertise, we have built a reputation for
+          delivering products that blend{" "}
+          <a className=" font-bold ">advanced technology</a>,{" "}
+          <a className=" font-bold ">durable design</a>, and
+          <a className=" font-bold ">customer-focused innovation</a>.
         </div>
         <div
           className={` text-[#495057] text-center lg:text-start ${
-            window?.innerWidth < 1024 ? ` text-xs ` : ` text-xl `
+            window?.innerWidth < 1024 ? ` text-xs ` : ` text-2xl `
           } `}
         >
-          <p className=" font-semibold ">Trusted by Thousands</p>
-          Gion Air Coolers have earned numerous awards for their innovation,
-          performance, and reliability. Experience the cooling technology that
-          makes us a leader in the industry.
+          {/* <p className=" font-semibold ">Trusted by Thousands</p> */}
+          Our{" "}
+          <a className=" font-bold ">
+            state-of-the-art OEM manufacturing facility
+          </a>
+          , supported by{" "}
+          <a className=" font-bold ">in-house R&D and a modern toolroom</a>,
+          ensures complete control over design, development, and quality
+          standards. Today, we proudly operate{" "}
+          <a className=" font-bold ">across Pan India</a>, supported by a wide
+          distribution network and trusted by{" "}
+          <a className=" font-bold ">millions of satisfied customers</a>.
         </div>
-        <Link to="/products" className=" text-gion-seafoam-green text-xs lg:text-xl font-semibold text-center ">
+        {/* <Link
+          to="/products"
+          className=" text-gion-seafoam-green text-xs lg:text-xl font-semibold text-center "
+        >
           {" "}
           Explore Gion Air Cooler...{" "}
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
@@ -182,21 +212,92 @@ function ContentAboutUssection2(params) {
 function AboutUs(params) {
   return (
     <div className=" mt-10 lg:mt-12 flex flex-col gap-3 lg:gap-[70px] items-center ">
+      <ContentAboutUs />
 
-      <div className="  w-screen h-fit flex flex-row justify-center  " >
-        <div className=" w-[80vw] lg:max-w-[62vw] h-[55vh] shadow-custom-shadow rounded-2xl lg:rounded-[44.5px] overflow-clip " >
-
-          <img src={abbanner} className=" relative lg:left-0  h-full lg:w-full object-cover " />
+      <div className="  w-screen h-fit flex flex-row justify-center  ">
+        <div className=" w-[80vw] lg:max-w-[62vw] h-[55vh] shadow-custom-shadow rounded-2xl lg:rounded-[44.5px] overflow-clip ">
+          <img
+            src={abbanner}
+            className=" relative lg:left-0  h-full lg:w-full object-cover "
+          />
         </div>
-
-
       </div>
 
-      <ContentAboutUs />
+      <div id="why choose us" className=" w-[70vw] flex flex-col gap-24 " >
+        <div className=" flex flex-col items-center leading-none ">
+          <div className=" font-urbanist font-bold text-gion-teal text-8xl uppercase ">
+            why choose us?
+          </div>
+          <div className=" font-thin text-[46px] ">
+            “Delivering comfort, efficiency, and trust in every product”
+          </div>
+        </div>
+
+        <div className=" flex flex-col its-center gap-24 ">
+          <div className=" flex flex-row gap-12 ">
+            <div className=" min-w-[679px] h-[451px] rounded-[57px] shadow-custom-shadow bg-neutral-200 overflow-clip ">
+              <img src={oem} className=" h-full object-cover " />
+            </div>
+            <div className=" flex flex-col justify-center gap-8 " >
+              <div className=" font-urbanist font-bold text-5xl text-gion-teal " >O.E.M. Excellence</div>
+              <div className=" text-4xl " >World-class manufacturing facilities delivering trusted quality.</div>
+            </div>
+          </div>
+
+
+
+          <div className=" flex flex-row-reverse gap-12 ">
+            <div className=" w-[679px] h-[451px] rounded-[57px] shadow-custom-shadow bg-neutral-200 overflow-clip ">
+              <img src={yrsexp} className=" h-full object-cover " />
+            </div>
+            <div className=" flex flex-col justify-center text-right gap-8 " >
+              <div className=" font-urbanist font-bold text-5xl text-gion-teal " >30+ Years of Experience</div>
+              <div className=" text-4xl " >Decades of innovation in cooling solutions.</div>
+            </div>
+          </div>
+
+
+
+          <div className=" flex flex-row gap-12 ">
+            <div className=" w-[679px] h-[451px] rounded-[57px] shadow-custom-shadow bg-neutral-200 overflow-clip ">
+              <img src={rnd} className=" h-full object-cover " />
+            </div>
+            <div className=" flex flex-col justify-center gap-8 " >
+              <div className=" font-urbanist font-bold text-5xl text-gion-teal " >In-House R&D & Toolroom</div>
+              <div className=" text-4xl " >Complete design-to-delivery expertise.</div>
+            </div>
+          </div>
+
+
+
+          <div className=" flex flex-row-reverse gap-12 ">
+            <div className=" w-[679px] h-[451px] rounded-[57px] shadow-custom-shadow bg-neutral-200 overflow-clip ">
+              <img src={natn} className=" h-full object-cover " />
+            </div>
+            <div className=" flex flex-col justify-center text-right gap-8 " >
+              <div className=" font-urbanist font-bold text-5xl text-gion-teal " >Nationwide Presence</div>
+              <div className=" text-4xl " >Trusted across India with a strong dealer network.</div>
+            </div>
+          </div>
+
+
+
+          <div className=" flex flex-row gap-12 ">
+            <div className=" w-[679px] h-[451px] rounded-[57px] shadow-custom-shadow bg-neutral-200 overflow-clip ">
+              <img src={happ} className=" h-full object-cover " />
+            </div>
+            <div className=" flex flex-col justify-center gap-8 " >
+              <div className=" font-urbanist font-bold text-5xl text-gion-teal " >Millions of Happy Customers</div>
+              <div className=" text-4xl " >A legacy of satisfaction and reliability.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/**---------------------------------------------------------------------------- */}
       {true && <ContentAboutUssection2 />}
       {/**---------------------------------------------------------------------------- */}
-      <div className=" relative px-4 py-5 lg:px-8 w-full h-[15vh] lg:h-[70vh] flex flex-col gap-3 lg:gap-16 items-center text-center overflow-clip ">
+      {/* <div className=" relative px-4 py-5 lg:px-8 w-full h-[15vh] lg:h-[70vh] flex flex-col gap-3 lg:gap-16 items-center text-center overflow-clip ">
         <div className=" lg:mt-52 text-[#FFFFFF] text-3xl lg:text-9xl [text-shadow:_-3px_3px_3.5px_rgb(0_0_0_/_50%)] ">
           OUR VISION
         </div>
@@ -208,7 +309,7 @@ function AboutUs(params) {
           src={vision}
           className=" absolute -mt-[260px] lg:-mt-[1200px] -z-10 "
         />
-      </div>
+      </div> */}
       <div className=" w-full ">
         <ContactUs />
       </div>
